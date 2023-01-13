d2e13a3751e79fdc1f13dd1a3c4f7ef9f5a98032
Michaela Stuchlíková <101055513+Monterea@users.noreply.github.com>
2023-01-13 20:11:04 +0100

    deleted injured sites

--- src/assets/text/infoText.js
+++
@@ -1,8 +0,0 @@
-export const FullName = "Ing. Michaela Stuchlíková";
-export const Street = "Jurkovičova 1551/4";
-export const City = "Karviná - Nové Město";
-export const PSC = "735 06";
-export const Phone = "+420 704 586 276";
-export const Email = "michaelastuchlikova9@gmail.com";
-export const ShortInfo =
-  "V roce 2022 jsem absolvovala rekvalifikační kurz Objektově orientovaného programování a tvorby www aplikací na Vysoké škole báňské v Ostravě.";
--- src/components/Fallback/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { PageContainer } from "../../App.mainStyles";
-
-export default function Fallback() {
-  return <PageContainer name="fallback page" />;
-}
